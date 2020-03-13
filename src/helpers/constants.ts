65a090a329b7a0ed0f332783dedbca225598d17a
Aditya Mudgerikar <aditya18thm@gmail.com>
2020-03-13 14:14:49 +0530

    Removed Redundant Constants

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -2,14 +2,3 @@ export const CONSTANTS = {
 	INPUT_FILE_BASE: 'input/',
 	OUTPUT_FILE_BASE: 'output/',
 };
-
-export const POINTS: { [key: string]: number | null } = {
-	T: 50,
-	H: 70,
-	_: 100,
-	X: 120,
-	'+': 150,
-	'*': 200,
-	'~': 800,
-	'#': null,
-};
